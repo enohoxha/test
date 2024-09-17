db2bcef6585dc2c45bb0e82bbc5f73f2286a3501
enohoxha <eno4hoxha@gmail.com>
2024-09-17 10:44:24 +0200

    Add funcion

---
+++ test.py
@@ -0,0 +1,10 @@
+def check_even_odd(number):
+    if number % 2 == 0:
+        return f"{number} is even."
+    else:
+        return f"{number} is odd."
+
+
+# Example usage:
+print(check_even_odd(4))  # Output: 4 is even.
+print(check_even_odd(7))  # Output: 7 is odd.
